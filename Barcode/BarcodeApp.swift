f83f12b845f04c25b1b4c105d6ee61da6fbd1f0d
Julian <julian.martinez.s@outlook.com>
2020-12-23 16:13:58 -0600

    Initial Commit

---
+++ Barcode/BarcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeApp.swift
+//  Barcode
+//
+//  Created by Julian Martinez on 12/23/20.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
